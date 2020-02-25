2108ae2a984521eddbc74cdfacd96faa4fdb3698
samlo <samlo@DESKTOP-I1VJ7AR.sdale.org>
2020-02-25 10:26:05 -0600

    deleted a comment

--- Governor's Coding Comp Libraries/src/CustomLibDRAKE.java
+++ Governor's Coding Comp Libraries/src/CustomLibDRAKE.java
@@ -26,7 +26,6 @@ public class CustomLibDRAKE {
             for (int j = 0; j < n-i-1; j++) 
                 if (arr[j] > arr[j+1]) 
                 { 
-                    // swap arr[j+1] and arr[i] 
                     int temp = arr[j]; 
                     arr[j] = arr[j+1]; 
                     arr[j+1] = temp; 
