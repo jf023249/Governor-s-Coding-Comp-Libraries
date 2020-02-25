cc40d031c4f8566e22d7ee1cc74bdc2e7c570085
samlo <samlo@DESKTOP-I1VJ7AR>
2020-02-24 21:40:33 -0600

    g

--- Governor's Coding Comp Libraries/src/CustomLibSAM.java
+++ Governor's Coding Comp Libraries/src/CustomLibSAM.java
@@ -266,7 +266,7 @@ public class CustomLibSAM {
 		ArrayList<Double> list = new ArrayList<Double>();
 		takeDoubleInput(list);
 		
-		System.out.println("goose");
+		System.out.println("gose");
 	}
 	
 	
