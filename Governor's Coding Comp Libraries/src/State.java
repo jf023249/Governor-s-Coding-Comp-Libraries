bd494134c9ad456135225c73d2ac36a234f26865
jf023249 <56647456+jf023249@users.noreply.github.com>
2020-02-26 17:29:04 -0600

    Renamed to State.java

--- Governor's Coding Comp Libraries/src/State.java
+++ Governor's Coding Comp Libraries/src/State.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
-public class CustomLibSAM {
+public class State {
 	
 	static String baseConversion(Integer number, int sBase, int dBase) 
 	{ 
