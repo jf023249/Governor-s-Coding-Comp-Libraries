15b5eefb5107efd222571cc572a157542abae5ce
BuildTools <unconfigured@null.spigotmc.org>
2020-02-27 11:36:53 -0600

    Added Encryption and Decryption via Vigenere Cipher with custom message and key.

--- Governor's Coding Comp Libraries/src/State.java
+++ Governor's Coding Comp Libraries/src/State.java
@@ -440,6 +440,54 @@ interface Collapse
 			
 			return formatter.format(d);
 		}
+	
+		static String vEncrypt(final String message, final String key)
+	    {
+
+	        String result = "";
+
+	        for (int i = 0, j = 0; i < message.length(); i++) {
+	            char c = message.charAt(i);
+	            if (Character.isLetter(c)){
+	                if(Character.isUpperCase(c)) {
+	                    result += (char) ((c + key.toUpperCase().charAt(j) - 2 * 'A') % 26 + 'A');
+
+	                } else {
+	                    result += (char) ((c + key.toLowerCase().charAt(j) - 2 * 'a') % 26 + 'a');
+
+	                }
+	            } else {
+	                result+=c;
+	            }
+	            j = ++j % key.length();
+	        }
+	        return result;
+	    }
+
+		static String vDecrypt(final String message, final String key)
+	    {
+	        String result ="";
+
+	        for(int i = 0, j = 0; i < message.length(); i++){
+
+	            char c = message.charAt(i);
+	            if (Character.isLetter(c)){
+	                if(Character.isUpperCase(c)) {
+	                    result += ((char)('Z'-(25-(c-key.toUpperCase().charAt(j)))%26));
+
+	                } else {
+	                    result += ((char)('z'-(25-(c-key.toLowerCase().charAt(j)))%26));
+
+	                }
+	            } else {
+	                result+=c;
+	            }
+
+	            j = ++j % key.length();
+
+	        }
+	        return result;
+	     }
 	}
 }
 
@@ -447,7 +495,7 @@ public class State implements Collapse {
 	
 	public static void main(String[] args)
 	{
-		System.out.println(StateMethods.rand('A','z'));
+		System.out.println(StateMethods.vDecrypt("Nszds hvwvk! Zsc oji mcm zcrsc?","goose"));
 	}
 	
 }
